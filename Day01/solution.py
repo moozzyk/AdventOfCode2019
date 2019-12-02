d9305e66367beb405f6840f8d95bf288c906e0af
moozzyk <moozzyk@gmail.com>
2019-12-01 19:53:30 -0800

    Day 2, Problem 2

--- Day01/solution.py
+++ Day01/solution.py
@@ -1,6 +1,28 @@
-file = open("input.txt", "r") 
-sum = 0
-for line in file: 
-  sum += int(int(line)/3) - 2      
+def get_fuel_requirement(mass):
+    return int(mass/3) - 2
 
-print(sum)
+
+def problem1(lines):
+    sum = 0
+    for line in lines: 
+        sum += get_fuel_requirement(int(line)) 
+    return sum
+
+
+def problem2(lines):
+    result = 0
+    for line in lines:
+        mass = int(line)
+        while mass > 6:
+            fuel_requirement = get_fuel_requirement(mass)
+            result += fuel_requirement
+            mass = fuel_requirement
+    return result
+
+
+with open("input.txt", "r") as f:
+    lines = list(f)
+    print(problem1(lines))
+    print(problem2(lines))
+
+
