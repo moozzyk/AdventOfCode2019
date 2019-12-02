f135408b4729d1d55976f7028ae5de13f6629842
moozzyk <moozzyk@gmail.com>
2019-12-01 22:23:43 -0800

    Day 2, Problem 2

--- Day02/solution.py
+++ Day02/solution.py
@@ -1,19 +1,34 @@
-def problem1(program):
-    program[1] = 12
-    program[2] = 2
+def run_program(program, noun, verb):
+    program[1] = noun
+    program[2] = verb
     ip = 0
     while ip < len(program):
         if program[ip] == 1:
-            program[program[ip + 3]] = program[program[ip + 1]] + program[program[ip + 2]]
+            program[program[ip + 3]] = program[program[ip + 1]] + \
+                program[program[ip + 2]]
         elif program[ip] == 2:
-            program[program[ip + 3]] = program[program[ip + 1]] * program[program[ip + 2]]
+            program[program[ip + 3]] = program[program[ip + 1]] * \
+                program[program[ip + 2]]
         elif program[ip]:
-            print(program)
             return program[0]
         ip += 4
+    return -1
+
+
+def problem1(program):
+    return run_program(program, 12, 2)
+
+
+def problem2(program):
+    for noun in range(0, 100):
+        for verb in range(0, 100):
+            result = run_program(program.copy(), noun, verb)
+            if result == 19690720:
+                return 100 * noun + verb
     return -1
 
 
 with open("input.txt", "r") as f:
     input = list(map(lambda n: int(n), f.readline().split(",")))
     print(problem1(input.copy()))
+    print(problem2(input.copy()))
