9247e7807977c5b610964d4ee47047c99b6587a3
moozzyk <moozzyk@gmail.com>
2019-12-03 22:40:21 -0800

    Day 4, Problem 2

--- Day04/solution.py
+++ Day04/solution.py
@@ -7,24 +7,44 @@ def has_consecutive(number):
     return False
 
 
-def recurse(min, max, number, digit):
+def has_2_consecutive(number):
+    i = 0
+    while i < len(number) - 1:
+        num_consecutive = 0
+        while i + num_consecutive < len(number) and number[i] == number[
+                i + num_consecutive]:
+            num_consecutive += 1
+
+        if num_consecutive == 2:
+            return True
+
+        i += num_consecutive
+
+    return False
+
+
+def recurse(min, max, number, digit, req_fn):
     if (len(number) > len(max)):
         return 0
 
     count = 0
     if len(number) > 0 and number[0] != '0' and int(number) >= int(
-            min) and int(number) <= int(max) and has_consecutive(number):
+            min) and int(number) <= int(max) and req_fn(number):
         count += 1
-        print(number)
 
     for d in range(digit, 10):
-        count += recurse(min, max, number + str(d), d)
+        count += recurse(min, max, number + str(d), d, req_fn)
 
     return count
 
 
 def problem1(min, max):
-    return recurse(str(min), str(max), "", 0)
+    return recurse(str(min), str(max), "", 0, has_consecutive)
+
+
+def problem2(min, max):
+    return recurse(str(min), str(max), "", 0, has_2_consecutive)
 
 
 print(problem1(137683, 596253))
+print(problem2(137683, 596253))
