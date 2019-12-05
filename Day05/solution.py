d93a0ca8567e3263821ef41e51cae324e864d25a
moozzyk <moozzyk@gmail.com>
2019-12-04 22:20:35 -0800

    Day 5, Problem 2
    Far from beautiful...

--- Day05/solution.py
+++ Day05/solution.py
@@ -25,12 +25,35 @@ def run_program(program, input):
         elif opcode == 4:
             reg = get_value(modes % 10, program, ip + 1)
             ip += 2
+        elif opcode == 5:
+            if get_value(modes % 10, program, ip + 1) != 0:
+                ip = get_value(modes // 10, program, ip + 2)
+            else:
+                ip += 3
+        elif opcode == 6:
+            if get_value(modes % 10, program, ip + 1) == 0:
+                ip = get_value(modes // 10, program, ip + 2)
+            else:
+                ip += 3
+        elif opcode == 7:
+            if get_value(modes % 10, program, ip + 1) < \
+                    get_value(modes // 10, program, ip + 2):
+                program[program[ip + 3]] = 1
+            else:
+                program[program[ip + 3]] = 0
+            ip += 4
+        elif opcode == 8:
+            if get_value(modes % 10, program, ip + 1) == \
+                    get_value(modes // 10, program, ip + 2):
+                program[program[ip + 3]] = 1
+            else:
+                program[program[ip + 3]] = 0
+            ip += 4
         elif opcode == 99:
             return reg
-        else:
-            print("invalid opcode: " + opcode)
 
 
 with open("input.txt", "r") as f:
     program = list(map(lambda n: int(n), f.readline().split(",")))
-    print(run_program(program, 1))
+    print(run_program(list(program), 1))
+    print(run_program(list(program), 5))
