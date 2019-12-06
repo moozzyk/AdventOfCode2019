88ce86aad7c887c64120e7bd89c1c9bd1d5a13f3
moozzyk <moozzyk@gmail.com>
2019-12-05 21:26:08 -0800

    Day 6, Problem 2

--- Day06/solution.py
+++ Day06/solution.py
@@ -9,22 +9,33 @@ def build_orbits(lines):
     return orbits
 
 
-def path_length(start, obrbits):
-    count = 0
+def get_path(start, obrbits):
+    path = []
     while start != "COM":
+        path.append(start)
         start = orbits[start]
-        count += 1
-    return count
+    return path
 
 
 def problem1(orbits):
     total_orbits = 0
     for k in orbits.keys():
-        total_orbits += path_length(k, orbits)
+        total_orbits += len(get_path(k, orbits))
     print(total_orbits)
+
+
+def problem2(orbits):
+    p1 = get_path("YOU", orbits)
+    p2 = get_path("SAN", orbits)
+    i, j = len(p1) - 1, len(p2) - 1
+    while p1[i] == p2[j]:
+        i -= 1
+        j -= 1
+    print(i + j)
 
 
 with open(sys.argv[1], "r") as f:
     lines = f.read().splitlines()
     orbits = build_orbits(lines)
     problem1(orbits)
+    problem2(orbits)
