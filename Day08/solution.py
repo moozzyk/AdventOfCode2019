203111cd40c9edda0a0238ff36983457e5ef6ab0
moozzyk <moozzyk@gmail.com>
2019-12-07 21:51:34 -0800

    Day 8, Problem 2

--- Day08/solution.py
+++ Day08/solution.py
@@ -13,6 +13,19 @@ def problem1(s):
     print(result[1] * result[2])
 
 
+def problem2(s):
+    image = ""
+    for pixel in range(150):
+        for layer in range(100):
+            pixel_value = int(s[layer * 150 + pixel])
+            if pixel_value != 2:
+                image += "*" if pixel_value == 1 else " "
+                break
+    for start_pos in range(0, 150, 25):
+        print(image[start_pos:start_pos + 25])
+
+
 with open(sys.argv[1], "r") as f:
     line = f.readline()
     problem1(line)
+    problem2(line)
