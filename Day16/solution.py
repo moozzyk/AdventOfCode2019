88fde0cd3177f8fad95af4480e3856feb53021d6
moozzyk <moozzyk@gmail.com>
2020-01-04 17:15:34 -0800

    Day 16, Problem 2

--- Day16/solution.py
+++ Day16/solution.py
@@ -1,29 +1,48 @@
 import sys
 
 
-def run_phase(input, phase):
-    pattern = [0, 1, 0, -1]
+def get_number(seq):
+    return int("".join(map(str, seq)))
+
+
+def run_phase(input_seq):
+    sums = [0]
+    for n in input_seq:
+        sums.append(sums[-1] + n)
     out = []
-    for idx in range(len(input)):
-        pattern_idx = 0
-        repeat_pattern = idx
-        new_value = 0
-        for digit in input:
-            if repeat_pattern == 0:
-                repeat_pattern = idx + 1
-                pattern_idx = (pattern_idx + 1) % len(pattern)
-            new_value += int(digit) * pattern[pattern_idx]
-            repeat_pattern -= 1
-        out.append(str(abs(new_value) % 10))
-    return "".join(out)
+    for digit_idx in range(len(input_seq)):
+        multiplier = 1
+        group_start = digit_idx
+        group_size = digit_idx + 1
+        digit = 0
+        while group_start < len(input_seq):
+            group_end = min(len(input_seq), group_start + group_size)
+            partial_sum = sums[group_end] - sums[group_start]
+            digit += partial_sum * multiplier
+            multiplier *= -1
+            group_start += group_size * 2
+        out.append(abs(digit) % 10)
+    return out
 
 
 def problem1(input_seq):
-    for phase in range(1, 101):
-        input_seq = run_phase(input_seq, phase)
-    print("".join(input_seq)[:8])
+    for _ in range(100):
+        input_seq = run_phase(input_seq)
+    print(get_number(input_seq[:8]))
+
+
+def problem2(input_seq):
+    offset = get_number(input_seq[:7])
+    print(offset)
+    long_input = input_seq * 10000
+    for _ in range(100):
+        for idx in range(len(long_input) - 2, offset - 1, -1):
+            long_input[idx] = (long_input[idx] + long_input[idx + 1]) % 10
+    print(get_number(long_input[offset:offset + 8]))
 
 
 with open(sys.argv[1], "r") as f:
     line = f.read().splitlines()[0]
-    problem1(line)
+    input_seq = list(map(int, line))
+    problem1(input_seq)
+    problem2(input_seq)
