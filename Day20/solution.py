02a839c897af934b874ad5ac4b7a7d8c979badd2
moozzyk <moozzyk@gmail.com>
2020-01-15 23:09:44 -0800

    Day 20, Problem 2

--- Day20/solution.py
+++ Day20/solution.py
@@ -1,5 +1,6 @@
 import sys
 
+
 def find_portals(plan):
     portals = {}
     for row in range(len(plan)):
@@ -11,9 +12,9 @@ def find_portals(plan):
                 if plan[row + 1][col].isupper():
                     portal_id = plan[row + 1][col] + plan[row + 2][col]
                 if plan[row][col - 1].isupper():
-                    portal_id = f"{plan[row][col - 2]}{plan[row][col - 1]}"
+                    portal_id = plan[row][col - 2] + plan[row][col - 1]
                 if plan[row][col + 1].isupper():
-                    portal_id = f"{plan[row][col + 1]}{plan[row][col + 2]}"
+                    portal_id = plan[row][col + 1] + plan[row][col + 2]
 
                 if portal_id:
                     s = portals.get(portal_id, [])
@@ -22,38 +23,91 @@ def find_portals(plan):
     return portals
 
 
-def shortest_path(start, end, plan, portals):
+def get_portals(plan):
+    portal_list = find_portals(plan)
+    start = portal_list.pop("AA", None)[0]
+    end = portal_list.pop("ZZ", None)[0]
+    portals = {}
+    debug = {}
+    for portal_name in portal_list:
+        (p1, p2) = portal_list[portal_name]
+        portals[p1] = p2
+        portals[p2] = p1
+        debug[p1] = portal_name
+        debug[p2] = portal_name
+    portals[end] = end
+    return (portals, start, end, debug)
+
+
+def shortest_path_to_portal(start, plan, portals):
     queue = [(start, 0)]
     visited = set()
+    exits = []
     while queue:
         (position, steps) = queue.pop(0)
         (row, col) = position
         if position in visited or plan[row][col] != ".":
             continue
         visited.add(position)
-        if position == end:
-            return steps
         queue.append(((row - 1, col), steps + 1))
         queue.append(((row + 1, col), steps + 1))
         queue.append(((row, col - 1), steps + 1))
         queue.append(((row, col + 1), steps + 1))
-        portal_pos = portals.get((row, col), None)
-        if portal_pos:
-            queue.append((portal_pos, steps + 1))
-    assert("Could not reach end")
+        if portals.get((row, col), None) and (row, col) != start:
+            exits.append(((row, col), steps))
+
+    return exits
 
 
 def problem1(plan):
-    portal_list = find_portals(plan)
-    start = portal_list.pop("AA", None)[0]
-    end = portal_list.pop("ZZ", None)[0]
-    portals = {}
-    for (p1, p2) in portal_list.values():
-        portals[p1] = p2
-        portals[p2] = p1
+    (portals, start, end, _) = get_portals(plan)
 
-    steps = shortest_path(start, end, plan, portals)
-    print(steps)
+    queue = [(start, 0)]
+    visited = set()
+    while queue:
+        (position, steps) = queue.pop(0)
+        if position in visited:
+            continue
+        visited.add(position)
+        if position == end:
+            print(steps - 1)  # ZZ is counted as portal
+            return
+        new_pos = map(lambda p: (portals[p[0]], steps + p[1] + 1),
+                      shortest_path_to_portal(position, plan, portals))
+        queue.extend(new_pos)
+
+
+def problem2(plan):
+    (portals, start, end, debug) = get_portals(plan)
+    min_steps = 10000000
+    queue = [(start, 0, 0)]
+    visited = {}
+    while queue:
+        (position, level, steps) = queue.pop(0)
+        if visited.get((position, level), 1000000) < steps:
+            continue
+        if (position == end and level != 0) or steps >= min_steps:
+            continue
+        visited[(position, level)] = steps
+        for p in shortest_path_to_portal(position, plan, portals):
+            ((row, col), seg_steps) = p
+            if (row, col) == end:
+                if level == 0:
+                    min_steps = min(min_steps, steps + seg_steps - 1)
+                continue
+            portal_exit = portals[(row, col)]
+            if row == 2 or row == len(plan) - 3 or \
+                    col == 2 or col == len(plan[2]) - 3:
+                if level != 0:
+                    queue.append(
+                            (portal_exit, level - 1, steps + seg_steps + 1))
+            else:
+                queue.append((portal_exit, level + 1, steps + seg_steps + 1))
+
+    print(min_steps + 1)
+
 
 with open(sys.argv[1], "r") as f:
-    problem1(f.readlines())
+    plan = list(f.read().splitlines())
+    problem1(plan)
+    problem2(plan)
