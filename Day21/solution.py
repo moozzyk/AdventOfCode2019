6e53b198d7e78e333d8d6d3c1ae4631c06382774
moozzyk <moozzyk@gmail.com>
2020-01-18 21:45:41 -0800

    Day 21, Problem 2

--- Day21/solution.py
+++ Day21/solution.py
@@ -2,6 +2,18 @@ import sys
 from intcode import Intcode
 
 
+def execute(program, script):
+    cpu = Intcode(program, [], [])
+    for line in script:
+        cpu.input.extend(list(map(ord, line + "\n")))
+
+    cpu.run()
+    if cpu.output[-1] < 128:
+        print("".join(map(chr, cpu.output)))
+    else:
+        print(cpu.output[-1])
+
+
 def problem1(program):
     script = [
             "NOT A T",
@@ -10,14 +22,33 @@ def problem1(program):
             "OR T J",
             "WALK",
             ]
-    cpu = Intcode(program)
-    for line in script:
-        cpu.input.extend(list(map(ord, line + "\n")))
+    execute(program, script)
 
-    cpu.run()
-    print(cpu.output[-1])
+
+def problem2(program):
+    script = [
+            "NOT A J",
+            "NOT J J",
+            "AND B J",
+            "AND C J",
+            "NOT J J",
+            "AND D J",
+
+            "NOT E T",
+            "NOT T T",
+            "AND I T",
+            "OR H T",
+            "AND T J",
+
+            "NOT A T",
+            "OR T J",
+
+            "RUN",
+            ]
+    execute(program, script)
 
 
 with open(sys.argv[1], "r") as f:
     program = list(map(lambda n: int(n), f.readline().split(",")))
     problem1(program)
+    problem2(program)
